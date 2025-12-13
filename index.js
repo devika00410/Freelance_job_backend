bfc556e51932ebf9739709f63d87fbe382a2fc02
Devika Anilkumar <devikaanilkumar053@gmail.com>
2025-12-13 13:27:41 +0530

    made few changes

--- index.js
+++ index.js
@@ -11,10 +11,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // ---------- CORS ----------
+
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
-    credentials: true
+    origin: [
+        'https://freelance-job-frontend.onrender.com',
+        'http://localhost:5173', 
+        'http://localhost:5174',
+        'http://localhost:3000',  
+        'http://localhost:10000'  
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
+
+// Handle OPTIONS requests for preflight
+app.options('*', cors()); 
 
 // ---------- Connect to MongoDB ----------
 connectDB().then(() => {
@@ -56,12 +68,19 @@ const startServer = () => {
 
     // 2. Initialize Socket.IO
     const io = socketIo(server, {
-        cors: {
-            origin: ['http://localhost:5173', 'http://localhost:5174'],
-            methods: ["GET", "POST", "PUT", "DELETE"],
-            credentials: true
-        }
-    });
+    cors: {
+        origin: [
+            'https://freelance-job-frontend.onrender.com',
+            'http://localhost:5173', 
+            'http://localhost:5174',
+            'http://localhost:3000',
+            'http://localhost:10000'
+        ],
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        credentials: true,
+        allowedHeaders: ['Content-Type', 'Authorization']
+    }
+});
 
     // 3. Socket.IO connection events
     io.on('connection', (socket) => {
